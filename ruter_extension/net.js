c94be0cef5d44d0e1284d7dc0c9cc64834a3d8b9
Karin <karinaas@gmail.com>
2013-03-10 19:43:06 +0100

    added xhr handling of closest stops

--- ruter_extension/net.js
+++ ruter_extension/net.js
@@ -1,18 +1,25 @@
 var xhr;
 
-//Sørli: 3010830
-var url = "http://reis.trafikanten.no/reisrest/realtime/getrealtimedata/3010830";
-ProcessJson(url);
+(function run(){	
+	var url = "http://reis.trafikanten.no/reisrest/realtime/getrealtimedata/3010830";
+	ProcessJson(url, processRealtime);
+})();
 
-function ProcessJson(url) {		
+function ProcessJson(url, onchange) {		
 	xhr = new XMLHttpRequest();
 	xhr.open("GET", url, true);
-	xhr.onreadystatechange = processChange;
+	xhr.onreadystatechange = onchange;
 	xhr.send(null);					
 }
 
-function processChange() {
+function processRealtime() {
 	if (xhr.readyState == 4 && xhr.status == 200) {		 
 		printTime();
+	} 
+}
+
+function processClosestStops() {
+	if (xhr.readyState == 4 && xhr.status == 200) {		 
+		findStops();
 	} 
 }
