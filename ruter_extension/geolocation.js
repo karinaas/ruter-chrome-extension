68aceabc5f2caf05bc4f6d3cef4c59e65721ecd3
Karin <karinaas@gmail.com>
2013-03-10 21:13:07 +0100

    selectbox showing closest stops.

--- ruter_extension/geolocation.js
+++ ruter_extension/geolocation.js
@@ -7,18 +7,43 @@ function showPosition(position) {
 	var utm = latLng.toUTMRef();
 		
 	var X = utm.easting.toString().split(".")[0];	
-	var Y = utm.northing.toString().split(".")[0];	
-
-	console.log("X: ", X);
-	console.log("Y: ", Y);
+	var Y = utm.northing.toString().split(".")[0];		
 
 	var url = "http://api-test.trafikanten.no/Place/GetClosestStopsByCoordinates/?coordinates=(X="+X+",Y="+Y+")&proposals=4"
 	ProcessJson(url, processClosestStops);
 }
 
-function findStops() {
+function listStops() {
 	var data = JSON.parse(xhr.responseText);	
-	for (key in data) {
-		console.log("KEY: ", data[key]);
-	}
+
+	var select = document.createElement("select");
+   	select.setAttribute("name", "stops");
+   	select.setAttribute("id", "stops");
+   	select.style.width = "200px";
+
+   	var option;
+
+   	for (key in data) {
+	   	option = document.createElement("option");
+	  	option.setAttribute("value", data[key].ID);
+	  	option.innerHTML = data[key].Name;
+	  	select.appendChild(option);
+	}	
+
+	var overlay = document.getElementById("overlay");
+	overlay.appendChild(select);	
+
+	setStopInLocalStorage();		
+
+	select.onchange = function() {
+		setStopInLocalStorage();	
+		var selectElement = document.getElementById("stops");			
+		selectElement.style.display = "none";
+		run();
+	};
+}
+
+function setStopInLocalStorage() {
+	var selectElement = document.getElementById("stops");
+	localStorage.stop = selectElement.options[selectElement.selectedIndex].value;		
 }
