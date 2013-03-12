9be0f1fa4ead7bed0517c0764e6b4e3621c257ba
Karin <karin@api.no>
2013-03-12 13:59:09 +0100

    converted tabs to spaces

--- ruter_extension/net.js
+++ ruter_extension/net.js
@@ -1,26 +1,26 @@
 var xhr;
 
 function run() {	
-	console.log("stop in local storage: ", localStorage.stop)
-	var url = "http://reis.trafikanten.no/reisrest/realtime/getrealtimedata/"+localStorage.stop;
-	ProcessJson(url, processRealtime);
+    console.log("stop in local storage: ", localStorage.stop)
+    var url = "http://reis.trafikanten.no/reisrest/realtime/getrealtimedata/"+localStorage.stop;
+    ProcessJson(url, processRealtime);
 };
 
 function ProcessJson(url, onchange) {		
-	xhr = new XMLHttpRequest();
-	xhr.open("GET", url, true);
-	xhr.onreadystatechange = onchange;
-	xhr.send(null);					
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = onchange;
+    xhr.send(null);
 }
 
 function processRealtime() {
-	if (xhr.readyState == 4 && xhr.status == 200) {		 
-		printTime();
-	} 
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        printTime();
+    }
 }
 
 function processClosestStops() {
-	if (xhr.readyState == 4 && xhr.status == 200) {		 
-		listStops();
-	} 
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        listStops();
+    }
 }
