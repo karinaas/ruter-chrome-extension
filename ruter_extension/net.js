e12555340391b5a4e0ebeed56c89935565462371
Karin <karinaas@gmail.com>
2013-03-14 22:09:15 +0100

    removed spaces

--- ruter_extension/net.js
+++ ruter_extension/net.js
@@ -7,12 +7,12 @@ function send (url, onSuccess) {
     var xhrRequest = xhr();
 
     xhrRequest.onreadystatechange = function onchange() {
-        if ( xhrRequest.readyState === 4 && xhrRequest.status == 200 ) {            
+        if (xhrRequest.readyState === 4 && xhrRequest.status == 200) {            
             onSuccess(JSON.parse(xhrRequest.responseText));
         }
     };
 
-    xhrRequest.open( "GET", url, true);
+    xhrRequest.open("GET", url, true);
     xhrRequest.send(null);
 
 }
