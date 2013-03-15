2b7d79efbb84b39ccde22eb1070b7645cf0bc767
Karin <karinaas@gmail.com>
2013-03-15 22:14:25 +0100

    added select information in first option in the selectbox

--- ruter_extension/geolocation.js
+++ ruter_extension/geolocation.js
@@ -1,61 +1,67 @@
-navigator.geolocation.getCurrentPosition(showPosition);
+function getPosition() {
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+getPosition();
 
 function showPosition(position) { 
-  	var lat = position.coords.latitude; 
-	var long = position.coords.longitude; 
-	var latLng = new LatLng(lat, long); 
-	var utm = latLng.toUTMRef();
-		
-	var X = utm.easting.toString().split(".")[0];	
-	var Y = utm.northing.toString().split(".")[0];		
-
-	var url = "http://api-test.trafikanten.no/Place/GetClosestStopsByCoordinates/?coordinates=(X="+X+",Y="+Y+")&proposals=4"
-	var testUrl = "http://api-test.trafikanten.no/Place/GetClosestStopsByCoordinates/?coordinates=(X=597982,Y=6643115)&proposals=100";	
-
-	send(url,
+    var lat = position.coords.latitude; 
+    var long = position.coords.longitude; 
+    var latLng = new LatLng(lat, long); 
+    var utm = latLng.toUTMRef();
         
-        function success(obj) {
-        	listStops(obj)
-		}
-	);
+    var X = utm.easting.toString().split(".")[0];   
+    var Y = utm.northing.toString().split(".")[0];      
+
+    var url = "http://api-test.trafikanten.no/Place/GetClosestStopsByCoordinates/?coordinates=(X="+X+",Y="+Y+")&proposals=4"
+    var testUrl = "http://api-test.trafikanten.no/Place/GetClosestStopsByCoordinates/?coordinates=(X=597982,Y=6643115)&proposals=100";  
+
+    send(url,
+        
+        function success (obj) {
+            listStops(obj)
+        }
+    );
 }
 
 function listStops(data) {
-	
-	var select = document.createElement("select");
-   	select.setAttribute("name", "stops");
-   	select.setAttribute("id", "stops");
-   	select.style.width = "200px";
+    
+    var select = document.createElement("select");
+    select.setAttribute("name", "stops");
+    select.setAttribute("id", "stops");
+    select.style.width = "200px";       
 
-   	var option;
+    var option;
+    option = document.createElement("option");
+    option.innerHTML = "VELG STOPPESTED";
+    select.appendChild(option);
 
-   	for (key in data) {
-	   	option = document.createElement("option");
-	  	option.setAttribute("value", data[key].ID);
-	  	option.innerHTML = data[key].Name;
-	  	select.appendChild(option);
-	}	
+    for (key in data) {
+        option = document.createElement("option");
+        option.setAttribute("value", data[key].ID);
+        option.innerHTML = data[key].Name;
+        select.appendChild(option);
+    }   
 
-	var overlay = document.getElementById("overlay");
-	overlay.appendChild(select);	
+    var overlay = document.getElementById("overlay");
+    overlay.appendChild(select);            
 
-	setStopInLocalStorage();		
-
-	select.onchange = function() {
-		setStopInLocalStorage();	
-		var selectElement = document.getElementById("stops");			
-		selectElement.style.display = "none";
-		
-		send("http://reis.trafikanten.no/reisrest/realtime/getrealtimedata/"+localStorage.stop,
+    select.onchange = function() {
+        setStopInLocalStorage();    
+        var selectElement = document.getElementById("stops");           
+        document.getElementById("overlay").removeChild(selectElement);
         
-	        function success(obj) {
-	        	printTime(obj)
-			}
-		);
-	};
+        send("http://reis.trafikanten.no/reisrest/realtime/getrealtimedata/"+localStorage.stopId,
+        
+            function success(obj) {
+                printTime(obj)
+            }
+        );
+    };
 }
 
 function setStopInLocalStorage() {
-	var selectElement = document.getElementById("stops");
-	localStorage.stop = selectElement.options[selectElement.selectedIndex].value;		
+    var selectElement = document.getElementById("stops");
+    localStorage.stopId = selectElement.options[selectElement.selectedIndex].value; 
+    localStorage.stopName = selectElement.options[selectElement.selectedIndex].innerHTML;       
 }
