9f2b620dbf6125a3d0db4c9a04ad0a578b111163
karinaas <karinaas@gmail.com>
2013-03-25 16:12:32 +0100

    moved time-stuff into its own file

---
+++ ruter_extension/time.js
@@ -0,0 +1,42 @@
+function getTimeUntilDeparture(date) {
+    var dateNow = new Date();
+    return date - dateNow.getTime();
+}
+
+function checkIfOneMinuteLeft(timeUntilDeparture) {
+    if (timeUntilDeparture <= 60000) {
+        return true;
+    }
+}
+
+function checkIfMoreThanTenMinutesLeft(timeUntilDeparture) {
+    if (timeUntilDeparture >= 600000) {
+        return true;
+    }
+}
+
+function checkIfOneMinutePastDeparture(timeUntilDeparture, element) {
+    if (timeUntilDeparture <= -60000) {
+        return true;
+    }
+}
+
+function createMinutes(amount) {
+    var out = "";
+
+    amount = Math.floor(amount / 1000);
+    amount = amount % 3600;
+
+    var minutes = Math.floor(amount / 60);//minutes
+    amount = amount % 60;
+
+    var seconds = Math.floor(amount);//seconds
+
+    out += minutes + " " + "min";
+    return out;
+
+}
+
+function fixMinutes(minutes) {
+    return (minutes.toString().length == 1 ? "0"+minutes.toString() : minutes);
+}
